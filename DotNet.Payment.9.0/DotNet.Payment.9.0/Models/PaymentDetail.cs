aa1c4ef3376667ede40799b9baf3b8ed8a094a42
Ronell Shivuri <RonellS@xiquelgroup.com>
2025-03-15 16:33:45 +0200

    Initial Commit

---
+++ DotNet.Payment.9.0/DotNet.Payment.9.0/Models/PaymentDetail.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DotNet.Payment._9._0.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; } = "";
+        [Column(TypeName = "nvarchar(16)")]
+        public string CardNumber { get; set; } = "";
+        [Column(TypeName = "nvarchar(6)")]
+        public string ExpirationDate { get; set; } = "";
+        [Column(TypeName = "nvarchar(3)")]
+        public string SecurityCode { get; set; } = "";
+
+    }
+}
